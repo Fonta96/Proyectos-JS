0a90b0bb8f714bf5fb25ae82bd75d37e1a92d71b
ivan fontanillo <ivanfontanillo@gmail.com>
2022-08-28 23:55:16 -0300

    Modificacion con Ciclo

--- js/Index.js
+++ js/Index.js
@@ -1,8 +1,11 @@
+//Constantes 
 
-let cuota3 = 1.28
-let cuota6 = 2.28
-let cuota12 = 3.28
-let iva = 1.24
+const cuota3 = 1.28
+const cuota6 = 2.28
+const cuota12 = 3.28
+const iva = 1.24
+
+//Funcion que calcula el valor del producto despues de las cuotas
 
 function revicionCuotas(cantidadCuotas,precioProducto){
 
@@ -20,14 +23,62 @@ function revicionCuotas(cantidadCuotas,precioProducto){
     
 }
 
+//Funcion que calcula el precio del productos con el iva
 
 function sumaIva(productos){
     productos *= iva
     return productos 
 }
 
+//Declaracion de listas
 
-let prod1 = parseFloat(prompt("Ingrese el valor del producto"))
+let listaProductos = []
+let listaCuotas = []
+let listaProductosIva = []
+let listaProductosCuota = []
+
+//ciclo while del input, lo que ingresa el usuario.
+
+let CargaDatos = true 
+while(CargaDatos === true) {
+    listaProductos.push([parseFloat(prompt("Ingrese el valor del producto")), parseFloat(prompt("Cuotas, 3,6 y 12"))])
+    let respuesta = parseInt(prompt("Desea agregar otro producto (1 si, 0 no)"))
+    if(respuesta === 1) {
+        CargaDatos = true 
+    }
+    else CargaDatos = false 
+}
+console.warn("Hasta aca nomas papu")
+
+// ciclo forEach de la lista de productos, para agregar el valor de la cuota al producto
+
+listaProductos.forEach(producto => {
+    listaProductosCuota.push(revicionCuotas(producto[1],producto[0]))
+});
+
+//ciclo forEach que agrega el valor del iva al producto
+
+listaProductosCuota.forEach(producto => {
+    listaProductosIva.push(sumaIva(producto))
+});
+
+//ciclo forEach que calcula el valor total e imprime el valor de cada producto 
+
+let valorTotal = 0
+listaProductosIva.forEach(producto => {
+    valorTotal += producto
+    document.write("El valor del producto es  ",  + producto + "<br>")
+});
+document.write("El valor total de los producto es  ",  + valorTotal)
+
+
+
+
+
+
+
+
+/*let prod1 = parseFloat(prompt("Ingrese el valor del producto"))
 let prod1cuotas = parseFloat(prompt("Cuotas, 3,6 y 12"))
 let prod2 = parseFloat(prompt("Ingrese valor del producto 2"))
 let prod2cuotas = parseFloat(prompt("Cuotas, 3,6 y 12"))
@@ -47,7 +98,7 @@ let valorTotal = prod1 + prod2 + prod3
 document.write("El valor del producto 1 es  ",  + prod1 + "<br>")
 document.write("El valor del producto 2 es  ",  + prod2 + "<br>")
 document.write("El valor del producto 3 es  ",  + prod3 + "<br>")
-document.write("El valor total de los productos ", + valorTotal)
+document.write("El valor total de los productos ", + valorTotal)*/
 
 
 
