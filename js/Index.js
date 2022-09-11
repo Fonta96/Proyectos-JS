ce26fd816ed32c1f216382e666c887a195c13948
ivan fontanillo <ivanfontanillo@gmail.com>
2022-09-10 23:30:31 -0300

    Eventos + interaccion HTML

--- js/Index.js
+++ js/Index.js
@@ -1,46 +1,11 @@
-//Constantes 
-
+//listas
+const carritoDeCompras = []
+const mensajeBienvenida = ["¡Hola Bienvenidos!", "Hola soy Goku" , "Welcome to the Jungle"]
+//declaracion de constantes
 const cuota3 = 1.28
 const cuota6 = 2.28
 const cuota12 = 3.28
 const iva = 1.24
-
-//Funcion que calcula el valor del producto despues de las cuotas
-
-function revicionCuotas(cantidadCuotas,precioProducto){
-
-    if (cantidadCuotas === 3) {
-        precioProducto *= cuota3
-    }
-    else if (cantidadCuotas === 6) {
-        precioProducto *= cuota6
-    }
-    else if (cantidadCuotas === 12) {
-        precioProducto *= cuota12
-    }
-    else document.write("Error al cargar la cuota <br>")
-    return precioProducto
-    
-}
-
-//Funcion que comprueba la existencia de un objeto en especifico EJ: 1 objeto llamado Celular.
-
-function existenProducto(){
-    let productoABuscar= prompt("Ingrese el producto a buscar:")
-    let existe = listaProductos.some ((producto)=> producto.nombre === productoABuscar)
-    if(existe) {
-        document.write("<br>Se encontro al menos 1 ",productoABuscar)
-    }else{
-        document.write("<br>No se encontro lo que buscaba")
-    }
-}
-
-//Funcion que calcula el precio del productos con el iva
-
-function sumaIva(productos){
-    productos *= iva
-    return productos 
-}
 
 //funcion constuctura de los productos
 
@@ -48,55 +13,76 @@ function objProducto(nombre,precio,cuota) {
     this.nombre = nombre
     this.precio = precio
     this.cuota = cuota
+    this.precioIva 
 }
 
-//Declaracion de listas
+//FUNCION DEL BUTTON
 
-let listaProductos = []
-let listaCuotas = []
-let listaProductosIva = []
-let listaProductosCuota = []
+function cargarProducto(nombre,precio,cuota) {
+    carritoDeCompras.push(new objProducto(nombre,precio,cuota))    
+}
 
-//ciclo while del input, lo que ingresa el usuario.
+//Funcion de testeo
 
-let CargaDatos = true 
-while(CargaDatos === true) {
-    let nombreProducto = prompt("Ingresa el nombre del producto")
-    let precioProducto = parseFloat(prompt("Ingresa el precio del producto"))
-    let cuotaProducto = parseFloat(prompt("Ingresa la cuota del producto: 3,6 o 12"))
-    const objetoProducto = new objProducto(nombreProducto,precioProducto,cuotaProducto)
-    listaProductos.push(objetoProducto)
-    let respuesta = parseInt(prompt("Desea agregar otro producto (1 si, 0 no)"))
-    if(respuesta === 1) {
-        CargaDatos = true 
+function imprimir() {
+     carritoDeCompras.forEach( producto => {
+        document.write(producto.nombre + " " + producto.precio + " " + producto.cuota + "<br>" )
+    });
+}
+
+//Funcion que calcula el valor del producto despues de las cuotas
+
+function revicionCuotas(){
+    carritoDeCompras.forEach( producto => {
+        producto.precioIva = producto.precio 
+
+        if (producto.cuota === 3) {
+            producto.precioIva *= cuota3
+        }
+        else if (producto.cuota === 6) {
+            producto.precioIva *= cuota6
+        }
+        else if (producto.cuota === 12) {
+            producto.precioIva *= cuota12
+        }
+        producto.precioIva *= iva
+    });
+    
+}
+
+//funcion para mensaje de bienvenida
+    function msjBienvenida() {
+       let numeroRandom = Math.floor(Math.random()*3)
+       document.getElementById('MensajeDeBienvenida').innerHTML += mensajeBienvenida[numeroRandom]
     }
-    else CargaDatos = false 
-}
-console.warn("Hasta aca nomas papu")
-
-// ciclo forEach de la lista de productos, para agregar el valor de la cuota al producto
-
-listaProductos.forEach(producto => {
-    listaProductosCuota.push(revicionCuotas(producto.cuota,producto.precio))
-});
-
-//ciclo forEach que agrega el valor del iva al producto
-
-listaProductosCuota.forEach(producto => {
-    listaProductosIva.push(sumaIva(producto))
-});
 
 //ciclo forEach que calcula el valor total e imprime el valor de cada producto 
+function eventHandler(event) {
+    if (event.keyCode === 13) {
+        revicionCompras()
+    }
+    else if (event.keyCode === 32) {
+        msjUsuario()
+    }
+    
+}
 
-let valorTotal = 0
-let contador = 0
-listaProductosIva.forEach(producto => {
-    valorTotal += producto
-    document.write("El valor del " + listaProductos[contador].nombre + " " + producto + "<br>")
-        contador++
-});
-document.write("El valor total de los producto es  ",  + valorTotal)
+//funcion forEach que calcula el valor total e imprime el valor de cada producto 
 
-// Se comprueba si existe un producto
+function revicionCompras() {
+    revicionCuotas()
+        let valorTotal = 0
+        carritoDeCompras.forEach(producto => {
+            valorTotal += producto.precioIva
+            document.write("El valor del " + producto.nombre + " Es: " + producto.precioIva + "<br>")
+        });
+        document.write("El valor total de los producto es  ",  + valorTotal)
+}
 
-existenProducto()
+//funcion para el mensaje del usuario
+function msjUsuario() {
+
+    let mensaje = prompt("Escribi tu mensaje aqui.")
+    mensaje += "<br>"
+    document.getElementById('mensajeUsuario').innerHTML += mensaje
+}
--- js/variables.js
+++
@@ -1,2 +0,0 @@
-/*let cantidadDeCuotas = parseInt(prompt("Ingrese la cantidad de Cuotas", cantidadDeCuotas))
-let intereses = 24*/
